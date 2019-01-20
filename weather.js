ab8c259310c12523fb6ec0bd7913344dacafd8a1
meanyMina <42491537+meanyMina@users.noreply.github.com>
2019-01-20 18:12:43 +0100

    #3.10 Getting the Weather part Two (API)

--- weather.js
+++ weather.js
@@ -1,6 +1,18 @@
-const API_KEY = "0595f2aede6994e6705b563d677c18a0";
+const weather = document.querySelector(".js-weather");
 
+const API_KEY = "0595f2aede6994e6705b563d677c18a0";
 const COORDS = "coords";
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    .then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}℃ @ ${place}`
+    }); // then은 data가 다 로딩되고 나서 함수를 호출하겠다. Data가 넘어오는 데 시간이 걸릴 수 있어서..
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +26,7 @@ function handleGeoSuccess(position){
         longitude//longitude: longitude를 줄인 것
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 
@@ -30,7 +43,8 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     } else {
-         // getWeather
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
